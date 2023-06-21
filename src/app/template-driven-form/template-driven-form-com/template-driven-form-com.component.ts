eb73756d369d6ca405bda6a22622dcbbc9c4a22f
akshaydeshmukh2455 <akshdeshmukh123@gmail.com>
2023-06-21 10:52:49 +0530

    first commit

---
+++ src/app/template-driven-form/template-driven-form-com/template-driven-form-com.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-driven-form-com',
+  templateUrl: './template-driven-form-com.component.html',
+  styleUrls: ['./template-driven-form-com.component.css']
+})
+export class TemplateDrivenFormComComponent {
+
+}
---
+++ src/app/template-driven-form/template-driven-form.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TemplateDrivenFormComComponent } from './template-driven-form-com/template-driven-form-com.component';
+
+
+
+@NgModule({
+  declarations: [
+    TemplateDrivenFormComComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TemplateDrivenFormModule { }
